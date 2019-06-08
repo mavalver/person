4b00f341fe937de07b48a8f657c464cc5f06634c
mvalver <mvalver@gmail.com>
2019-06-08 20:28:47 +0200

    Validations

--- model.js
+++ model.js
@@ -2,13 +2,20 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize("sqlite:db.sqlite", {logging: false});
 
-sequelize.define('person', 
+sequelize.define(
+    'person', 
     {
         name: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            unique: { msg: "Name already exists"},
+            validate: { is: { args: /^[a-z]+$/i, msg: "Invalid characters" }}
         },
         age: {
-            type: Sequelize.INTEGER
+            type: Sequelize.INTEGER,
+            validate: { 
+                min: { args: [0], msg: "Age less than 0"},
+                max: { args: [150], msg: "Age higher than 0"}
+            }
         }
     }
 );
